e11c40eee5e52ea713108ebdd40800b40314f6db
James Sgarella <Jimbo8702@gmail.com>
2023-08-13 22:40:09 -0400

    error handling, db package, user handlers

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/Jimbo8702/goreservation/api"
+	"github.com/Jimbo8702/goreservation/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config =fiber.Config{
+    ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+    },
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handler initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
-
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
--- types/user.go
+++ types/user.go
@@ -1,8 +1,9 @@
 package types
 
 type User struct {
-	ID string 	`bson:"_id" json:"id,omitempty"`
+	ID string 	`bson:"_id,omitempty" json:"id,omitempty"`
 	FirstName 	string `bson:"firstName" json:"firstName"`
 	LastName 	string `bson:"lastName" json:"lastName"`
 }
+
 
